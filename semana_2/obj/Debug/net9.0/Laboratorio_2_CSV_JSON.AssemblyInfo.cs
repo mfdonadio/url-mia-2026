56e3b250df1b0fe1e3fff7938b7e963ba5709bfb
Marco Donadio <mfdonadio@correo.url.edu.gt>
2026-07-24 09:57:56 -0600

    Feat: cambios y carpeta de laboratorio semana 3

--- semana_2/obj/Debug/net9.0/Laboratorio_2_CSV_JSON.AssemblyInfo.cs
+++ semana_2/obj/Debug/net9.0/Laboratorio_2_CSV_JSON.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio_2_CSV_JSON")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+470dfb8b547d500e84478c7793a433be5c1f8ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio_2_CSV_JSON")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio_2_CSV_JSON")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
